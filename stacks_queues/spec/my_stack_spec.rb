5eb9f39ed09a2df0bf133e807af060cd45301715
Ben Neely <ben@bloc.io>
2016-04-24 08:11:38 -0600

    Add some specs

--- stacks_queues/spec/my_stack_spec.rb
+++ stacks_queues/spec/my_stack_spec.rb
@@ -1,5 +1,36 @@
-require 'spec_helper'
+require_relative '../my_stack'
 
-describe MyStack do
+RSpec.describe MyStack do
+  it 'should know when it is empty' do
+    stack = MyStack.new
 
+    expect(stack.empty?).to(be(true))
+  end
+
+  it 'should push items onto the stack' do
+    stack = MyStack.new
+
+    stack.push(:item1)
+
+    expect(stack.empty?).to(be(false))
+  end
+
+  it 'should pop items off of the stack' do
+    stack = MyStack.new
+
+    stack.push(:item1)
+
+    expect(stack.pop).to(be(:item1))
+    expect(stack.empty?).to(be(true))
+  end
+
+  it 'should maintain LIFO priority' do
+    stack = MyStack.new
+
+    stack.push(:item1)
+    stack.push(:item2)
+
+    expect(stack.pop).to(be(:item2))
+    expect(stack.pop).to(be(:item1))
+  end
 end
--- stacks_queues/spec/queue_stack_spec.rb
+++ stacks_queues/spec/queue_stack_spec.rb
@@ -1,5 +1,38 @@
-require 'spec_helper'
+require_relative '../queue_stack'
 
-describe QueueStack do
+RSpec.describe QueueStack do
+  it 'should know when it is empty' do
+    stack = QueueStack.new
 
+    expect(stack.empty?).to(be(true))
+  end
+
+  it 'should push items onto the stack' do
+    stack = QueueStack.new
+
+    stack.push(:item1)
+
+    expect(stack.empty?).to(be(false))
+  end
+
+  it 'should pop items off of the stack' do
+    stack = QueueStack.new
+
+    stack.push(:item1)
+
+    expect(stack.pop).to(be(:item1))
+    expect(stack.empty?).to(be(true))
+  end
+
+  it 'should maintain LIFO priority' do
+    stack = QueueStack.new
+
+    stack.push(:item1)
+    stack.push(:item2)
+
+    p stack
+
+    expect(stack.pop).to(be(:item2))
+    expect(stack.pop).to(be(:item1))
+  end
 end
