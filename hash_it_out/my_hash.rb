02c6a2fe27e1e8c245f05a489bf33c61950c4707
Steven Schulte <shotgunsteven4@yahoo.com>
2016-05-03 21:39:15 -0600

    A working version, but inefficient

--- hash_it_out/my_hash.rb
+++ hash_it_out/my_hash.rb
@@ -1,39 +1,56 @@
 class MyHash
 
-    def array
-        @array
-    end
+  attr_accessor :array
 
   def initialize
-    @array = Array.new
+     @array = []
   end
 
-  def add(key, value)
-    @array[hash(key)] = value
-  end
-
-  def get(key)
-    @array[hash(key)]
-  end
-
+  # 1) Use a hashing function to generate unique, fixed-length hash codes.
   def hash(key)
     index = 0
     chars = key.split('')
-    chars.each do |char|
-      index += char.ord
+    chars.each.with_index do |char, i|
+      index = (i*char.ord + i*char.ord)
     end
     index
   end
+
+  # # 2) Expand the internal array to a prime number nearest the next
+  # #    power of 2 when you detect a collision, then reinsert all values.
+  # def new_size(size)
+  # end
+
+  # 3) Create insertion(my_hash[key] = value) by overriding the respective operators.
+  def insertion(key, value)
+    # 5) Store each key and value pair as one element in the array.
+    # k = @array.length
+    # if (k != 0)
+    #   true_index = hash(key) % k
+    # else
+    #   true_index = hash(key)
+    # end
+    @array[hash(key)] = value
+  end
+
+  # 4) Create lookup (value = my_hash[key]) by overriding the respective operators.
+  def lookup(key)
+    @array[hash(key)]
+  end
+
 end
 
 
-hash = MyHash.new
+# lotr_runtimes = MyHash.new
+#
+# lotr_runtimes.insertion('foo', 'bar')
+# lotr_runtimes.insertion('oof', 'yolo')
+#
+# puts lotr_runtimes.lookup('foo')
+# puts lotr_runtimes.lookup('oof')
+# # => 'bar'
 
-hash.add('foo', 'bar')
-p hash.array
-hash.add('oofs', 'yolo')
-p hash.array
-
-puts hash.get('foo')
-puts hash.get('oofs')
-# => 'bar'
+# lotr_runtimes.insertion("The Lord of the Rings: The Fellowship of the Ring", "3 hours, 48 minutes")
+# lotr_runtimes.insertion("The Lord of the Rings: The Two Towers","3 hours, 55 minutes")
+#
+# p lotr_runtimes.lookup('The Lord of the Rings: The Fellowship of the Ring')
